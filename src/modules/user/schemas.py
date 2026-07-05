1a550e38e0276784745fd99a81eb5bae93a4a59b
Matias Marcelo Revollo Cornejo <mati13752933@gmail.com>
2026-07-05 01:42:07 -0400

    feat: add user schemas

---
+++ src/modules/user/schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, ConfigDict, EmailStr
+from src.core.enums import TypeRoles
+class UserBase(BaseModel):
+    email: EmailStr
+    name: str
+    last_name: str
+    codsis: str
+class UserCreate(UserBase):
+    password: str
+class UserResponse(UserBase):
+    model_config=ConfigDict(from_attributes=True)
+    id: int
+    role: TypeRoles
+    area_id: int | None=None
+class UserPatch(BaseModel):
+    email: EmailStr | None=None
+    name: str | None= None
+    last_name: str | None=None
+    codsis: str | None=None
+    role: TypeRoles | None=None
+    area_id: int | None= None
