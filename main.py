879c36f66c0846d216d4c5f4d4dc433971e66b8c
Matias Marcelo Revollo Cornejo <mati13752933@gmail.com>
2026-07-05 15:14:38 -0400

    feat: create database tables

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from sqlalchemy import text
-import src.core.mapping_database
+import src.core.mapping_database  # noqa
 from src.core.api import api
-from src.core.data_base import engine
+from src.core.data_base import engine, create_database
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     async with engine.connect() as conn:
         await conn.execute(text("SELECT 1"))
+    await create_database()
     yield
 app = FastAPI(lifespan=lifespan)
 app.include_router(api)
