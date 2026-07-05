5797f26f5008027b30ed996bd7f02cd323704ac8
Matias Marcelo Revollo Cornejo <mati13752933@gmail.com>
2026-07-05 18:20:25 -0400

    feat: register stage model

--- src/core/mapping_database.py
+++ src/core/mapping_database.py
@@ -1,4 +1,5 @@
-from src.modules.area.model import Area, AreaTutor 
-from src.modules.session.model import Session  
-from src.modules.submission.model import Submission  
-from src.modules.user.model import User  
+from src.modules.area.model import Area, AreaTutor
+from src.modules.session.model import Session
+from src.modules.stage.model import Stage, StageTutor
+from src.modules.submission.model import Submission
+from src.modules.user.model import User
